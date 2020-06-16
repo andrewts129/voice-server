e80e04356398f8ed6ba4d65b7c4cb94383cb3961
Andrew Smith <andrewts129@gmail.com>
2020-06-16 13:22:35 -0400

    added wav generation

--- src/main/scala/io/andrewsmith/voiceserver/Main.scala
+++ src/main/scala/io/andrewsmith/voiceserver/Main.scala
@@ -1,22 +1,36 @@
 package io.andrewsmith.voiceserver
 
+import java.io.ByteArrayOutputStream
+
+import scala.concurrent.ExecutionContext.global
+import scala.sys.process._
+
 import cats.effect._
-import org.http4s.HttpRoutes
+import org.http4s.{HttpRoutes, MediaType}
 import org.http4s.dsl.io._
+import org.http4s.headers.`Content-Type`
 import org.http4s.implicits._
 import org.http4s.server.blaze._
-import scala.concurrent.ExecutionContext.global
 
 object Main extends IOApp {
-  private val helloWorldService = HttpRoutes.of[IO] {
-    case GET -> Root / "hello" / name =>
-      Ok(s"Hello, $name.")
+  private def getWav: Array[Byte] = {
+    val out = new ByteArrayOutputStream()
+    val exitCode = "text2wave test.txt" #> out !< ProcessLogger(_ => ())
+    exitCode match {
+      case 0 => out.toByteArray
+      case _ => throw new RuntimeException(s"error")
+    }
+  }
+
+  private val service = HttpRoutes.of[IO] {
+    case GET -> Root =>
+      Ok(getWav, `Content-Type`(MediaType.audio.wav))
   }.orNotFound
 
   override def run(args: List[String]): IO[ExitCode] =
     BlazeServerBuilder[IO](global)
       .bindHttp(3000)
-      .withHttpApp(helloWorldService)
+      .withHttpApp(service)
       .serve
       .compile
       .drain
