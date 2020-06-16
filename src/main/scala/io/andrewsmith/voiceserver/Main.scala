af2e0f3fd9e4c92016fccf846cede7d2e33d6514
Andrew Smith <andrewts129@gmail.com>
2020-06-16 18:33:01 -0400

    safer input

--- src/main/scala/io/andrewsmith/voiceserver/Main.scala
+++ src/main/scala/io/andrewsmith/voiceserver/Main.scala
@@ -1,10 +1,9 @@
 package io.andrewsmith.voiceserver
 
-import java.io.ByteArrayOutputStream
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
 
 import scala.concurrent.ExecutionContext.global
 import scala.sys.process._
-
 import cats.effect._
 import org.http4s.{HttpRoutes, MediaType}
 import org.http4s.dsl.io._
@@ -14,11 +13,13 @@ import org.http4s.server.blaze._
 
 object Main extends IOApp {
   private def getWav(text: String): Array[Byte] = {
-    val out = new ByteArrayOutputStream()
-    val exitCode = s"echo $text" #| "text2wave" #> out !< ProcessLogger(_ => ())
+    val inputStream = new ByteArrayInputStream(text.getBytes)
+    val outputStream = new ByteArrayOutputStream()
+
+    val exitCode = "text2wave" #< inputStream #> outputStream !< ProcessLogger(_ => ())
     exitCode match {
-      case 0 => out.toByteArray
-      case _ => throw new RuntimeException(s"error")
+      case 0 => outputStream.toByteArray
+      case _ => throw new RuntimeException(s"Nonzero exit value ($exitCode) for input: $text")
     }
   }
 
